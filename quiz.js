6e36f346de608b32ca7c89955ea6926d74d50f2d
isabelle <isabellehopeeriksson@gmail.com>
2026-05-09 00:17:03 -0700

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -12,7 +12,7 @@ const result = {
 };
 
 function showResult() {
-  const pattern = [0,1,2,3,4,5,6,7,8,9,10].map(i => document.getElementById('q'+i).checked ? 'T' : 'F').join('');
+  const pattern = [0,1,2,3,4,5,6,7,8,9].map(i => document.getElementById('q'+i).checked ? 'T' : 'F').join('');
 
   const isSeonghwa = pattern == "FFFFFFFFTT";
   const r = isSeonghwa ? result.seonghwa : result.hongjoong;
